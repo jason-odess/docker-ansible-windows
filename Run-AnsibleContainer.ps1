e01142ec0630e646ad9e6c92d01193b55f2f392c
jason-odess <jason.odess@libertymutual.com>
2018-12-28 08:46:40 -0500

    First Commit - PS script

---
+++ Run-AnsibleContainer.ps1
@@ -0,0 +1,79 @@
+# Necessary block to ignore Cert errors, during this session only
+# equivalent to 'Curl -K'
+# for what ever reason, the @" "@ operator doesn't allow whitespace ahead of the multiline string...
+function Set-CertPolicy{
+param()
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+}
+    
+    
+function Run-AnsibleContainer{
+    param (
+        [Parameter(Mandatory=$true)][string]$ucpFQDN,
+        [Parameter(Mandatory=$true)][string]$ucpAdmin,
+        [Parameter(Mandatory=$true)][string]$ucpPW,
+        [Parameter(Mandatory=$true)][string]$AnsibleImage,
+        [Parameter(Mandatory=$true)][string]$WindowsTarget,
+        [Parameter(Mandatory=$true)][string]$AnsibleUser,
+        [Parameter(Mandatory=$true)][string]$AnsiblePassword
+    )
+    $UCPurl = "https://" + $ucpFQDN 
+    
+    # Get Auth Token
+    $authEndpoint = "/auth/login"
+    $authURI = $UCPurl + $authEndpoint
+    $authBody = @{ "username" = $ucpAdmin ; "password" = $ucpPW } | ConvertTo-Json
+    $authtoken = Invoke-RestMethod -Uri $authURI -Body $authBody -Method Post
+    $authtoken = $authtoken.auth_token
+    
+    # Try to Run Container docker_ansible_test2:latest
+    $header = @{"Authorization" = "Bearer " + $authtoken}
+    
+    # Build API payload for CREATE
+    $endpoint = "/containers/create"
+    $URI = $UCPurl + $endpoint
+    $body = @{ 
+        "AttachStdin" = $false
+        "AttachStdout" = $true
+        "AttachStderr" = $true
+        "Image" = $ansibleImage  
+        "ENV" = @(
+            "WINDOWS_TARGET=$WindowsTarget",
+            "ANSIBLEUSER=$AnsibleUser",
+            "ANSIBLEPASSWORD=$AnsiblePassword"
+        )
+        "EntryPoint" = "/bin/bash"
+        "Cmd" = "/tmp/windows-ansible/windows-ansible-playbook/wrapper.sh"
+        "HostConfig" = @{ "LogConfig" = @{ "Type" = "json-file"}}
+    } | ConvertTo-Json
+    
+    # Create the container
+    $CreateResponse = Invoke-RestMethod -Uri $URI -Headers $header -Method Post -ContentType "application/json" -Body $body
+
+    # Build API Payload for START
+    $containerID = $CreateResponse.ID
+    $endpoint = "/containers/$containerID/start"
+    $URI = $UCPurl + $endpoint
+
+    # Start the container that we created
+    $StartResponse = Invoke-RestMethod -Uri $URI -Headers $header -Method Post -ContentType "application/json"
+    echo $StartResponse
+}
+
+# Ignore-Certs
+Set-CertPolicy
+
+Start-Sleep -s 5
+
+Run-AnsibleContainer
